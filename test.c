39869953c7d4cbf02698f4856d97c3f2ea4edcc9
jasonchanhh <jc15502@my.bristol.ac.uk>
2017-12-08 02:48:31 +0000

    test

--- test.c
+++ test.c
@@ -7,44 +7,28 @@
 
 int main (int argc, char *argv[]) {
 
-//   int a[N], b[N];
-//   int tid;
-//   int i,j;
-//
-//   for (i=0;i<N;i++) {
-//     a[i]=0;
-//     b[i]=rand();
-//   }
-//
-// #pragma omp parallel private(j, tid)
-//   tid = omp_get_thread_num();
-//   printf("Thread = %d\n", tid);
-//
-//   for(j=0; j<REPEAT; j++) {
+  int a[N], b[N];
+  int tid;
+  int i,j;
+
+  for (i=0;i<N;i++) {
+    a[i]=0;
+    b[i]=rand();
+  }
+
+#pragma omp parallel private(j, tid)
+  tid = omp_get_thread_num();
+  printf("Thread = %d\n", tid);
+
+  for(j=0; j<REPEAT; j++) {
+    printf("Thread[%d], a is : %d, at (%d)\n",tid, a[i], j);
 // #pragma omp for
 //     for (i=0; i<N; i++) {
 //       a[i] = b[i];
-//       printf("Thread[%d], a is : %d, at (%d,%d)\n",tid, a[i], i, j);
-//     }
-//   }
-int nthreads, tid;
-/* These are commonly used variable names - nthreads is short for Number of threads, while */
-/* tid is short for Thread ID. These are just conventions though, so you can use your own too */
+      // printf("Thread[%d], a is : %d, at (%d,%d)\n",tid, a[i], i, j);
+    }
+  }
 
-/* Fork a team of threads giving them their own copies of variables */
-#pragma omp parallel private(tid)
-{
-/* Obtain thread number */
-tid = omp_get_thread_num();
-printf("Hello, world from thread = %d\n", tid);
-
-/* Only master thread does this */
-#pragma omp master
-{
-  nthreads = omp_get_num_threads();
-  printf("Number of threads = %d\n", nthreads);
-}
-}  /* All threads join master thread and disband */
 
   return EXIT_SUCCESS;
 }
