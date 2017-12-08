bf804dc1df30782846e9734c7ebf7350406a2e94
jasonchanhh <jc15502@my.bristol.ac.uk>
2017-12-08 03:08:53 +0000

    test

--- test.c
+++ test.c
@@ -16,17 +16,17 @@ int main (int argc, char *argv[]) {
     b[i]=rand();
   }
 
-#pragma omp parallel private(tid)
+#pragma omp parallel private(j, tid)
   tid = omp_get_thread_num();
   printf("Thread = %d\n", tid);
 
   for(j=0; j<REPEAT; j++) {
     printf("Thread[%d], a is : %d, at (%d)\n",tid, a[i], j);
-#pragma omp for
-    for (i=0; i<N; i++) {
-      a[i] = b[i];
-      printf("Thread[%d], a is : %d, at (%d,%d)\n",tid, a[i], i, j);
-    }
+// #pragma omp for
+//     for (i=0; i<N; i++) {
+//       a[i] = b[i];
+//       printf("Thread[%d], a is : %d, at (%d,%d)\n",tid, a[i], i, j);
+//     }
   }
 
 
