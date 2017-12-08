4c890f6ba2aa8c351eb0f3035c0d546f86b833b2
jasonchanhh <jc15502@my.bristol.ac.uk>
2017-12-08 06:30:13 +0000

    forgot for

--- jacobi.c
+++ jacobi.c
@@ -58,7 +58,7 @@ int run(float *restrict A, float *restrict b, float *restrict x, float *restrict
     sqdiff = 0.0;
     // sumsqdiff = 0.0;
     // Perfom Jacobi iteration
-#pragma omp parallel reduction(+:sqdiff)
+#pragma omp parallel for reduction(+:sqdiff)
 //   {
 // #pragma omp for
     for (row = 0; row < N; row++)
