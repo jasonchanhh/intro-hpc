5d2574ba3dc6daaf764c2e5dd4f5b8fe131a9ae6
jasonchanhh <jc15502@my.bristol.ac.uk>
2017-12-09 16:02:28 +0000

    add schedule

--- jacobi.c
+++ jacobi.c
@@ -30,7 +30,7 @@ static int MAX_ITERATIONS;
 static int SEED;
 static double CONVERGENCE_THRESHOLD;
 
-#define CHUNKSIZE 1000
+#define CHUNKSIZE 100
 #define SEPARATOR "-------------------------------------\n"
 
 // Return the current time in seconds since the Epoch
